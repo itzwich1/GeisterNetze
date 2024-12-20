7cef56fb6200632647f5c314f8111cb2a9a52967
Felix Wich <fx.wich@outlook.de>
2024-12-20 21:35:10 +0100

    Meldung Formular hinterlegt

---
+++ src/main/java/com/geisternetze/beans/MeldungBean.java
@@ -0,0 +1,70 @@
+package com.geisternetze.beans;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+
+
+@Named
+@RequestScoped
+public class MeldungBean implements Serializable{
+
+
+
+    private boolean anonym;
+    private String vorname;
+    private String nachname;
+    private String telefonnummer;
+    private String breitengrad;
+    private String laengengrad;
+
+    public boolean isAnonym() {
+        return anonym;
+    }
+
+    public void setAnonym(boolean anonym) {
+        this.anonym = anonym;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getTelefonnummer() {
+        return telefonnummer;
+    }
+
+    public void setTelefonnummer(String telefonnummer) {
+        this.telefonnummer = telefonnummer;
+    }
+
+    public String getBreitengrad() {
+        return breitengrad;
+    }
+
+    public void setBreitengrad(String breitengrad) {
+        this.breitengrad = breitengrad;
+    }
+
+    public String getLaengengrad() {
+        return laengengrad;
+    }
+
+    public void setLaengengrad(String laengengrad) {
+        this.laengengrad = laengengrad;
+    }
+
+}
