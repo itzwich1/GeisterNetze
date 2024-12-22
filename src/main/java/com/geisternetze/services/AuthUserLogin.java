037a4518c265fdf67063151e9ac64b9f5a670bbd
Felix Wich <fx.wich@outlook.de>
2024-12-22 16:12:59 +0100

    Postgres sql DB dependency hinzugefuegt

--- src/main/java/com/geisternetze/services/AuthUserLogin.java
+++ src/main/java/com/geisternetze/services/AuthUserLogin.java
@@ -2,6 +2,14 @@ package com.geisternetze.services;
 
 public class AuthUserLogin {
 
+    private boolean validUser = false;
+
+    public void AuthUserLogin() {
+
+        //Hier die initialisierung des User durchführen
+    }
+
+
 
 
 }
