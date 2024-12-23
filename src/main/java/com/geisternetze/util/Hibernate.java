1333da182c89de40108e45f4f961766bf2d633f3
Felix Wich <fx.wich@outlook.de>
2024-12-23 18:47:37 +0100

    Datenbank scheint jetzt zu funktionieren.

---
+++ src/main/java/com/geisternetze/util/Hibernate.java
@@ -0,0 +1,31 @@
+package com.geisternetze.util;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+@ApplicationScoped
+public class Hibernate {
+    private final EntityManagerFactory entityManagerFactory;
+
+    static String PERSISTENCEUNITNAME = "MyPersistenceUnit" ;
+    public Hibernate() {
+        entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCEUNITNAME);
+    }
+
+    @Produces
+    @ApplicationScoped
+    public   EntityManagerFactory getEntityManagerFactory(){
+        return entityManagerFactory;
+    }
+
+    @Produces
+    @ApplicationScoped
+    public EntityManager createEntityManager() {
+
+        return    entityManagerFactory.createEntityManager();
+
+    }
+}
